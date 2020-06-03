3b5d7f6a51e3a7ac74f671c652c7d0cf1bcc1901
Shyroe <leo.henrique747@gmail.com>
2020-06-03 16:04:02 -0300

    migrations and seed and sqlite3 OK

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,11 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  console.log("list users");
-  res.json({
-    name: "leonardo lopes",
-    age: 35,
-  });
-});
+//Middlewares
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333);

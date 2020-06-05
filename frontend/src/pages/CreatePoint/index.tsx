e1881e6c3b7e00331384ed12c67f2cf7ea02bc0e
Shyroe <leo.henrique747@gmail.com>
2020-06-05 18:05:48 -0300

    integracao mapa finalizada

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import { Map, TileLayer, Popup, Marker } from "react-leaflet";
+import api from "../../services/api";
+import axios from "axios";
+
+// Sempre que criarmos um estado para um array (utilizando typescript) ou um objeto, nós precisamos informar manualmente o tipo da variável que será armazenada dentro desse estado
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+// useState<GENERIC>([])
 
 const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const [ufs, setUfs] = useState<string[]>([]); // ufs é do tipo array de strings
+  const [selectedUf, setSelectedUf] = useState("0");
+  console.log("items: ", items);
+
+  useEffect(() => {
+    api.get("items").then((res) => {
+      return setItems(res.data);
+      // return setItems([res.data]);
+    });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get<IBGEUFResponse[]>(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      )
+      .then((res) => {
+        const ufInitials = res.data.map((uf) => uf.sigla);
+        setUfs(ufInitials);
+        // console.log(ufInitials);
+      });
+  }, []);
+
+  // change UF
+  const handleSelectUf = () => {};
   return (
     <div id="page-create-point">
       <header>
@@ -39,16 +81,67 @@ const CreatePoint = () => {
             </div>
           </div>
         </fieldset>
+
         <fieldset>
           <legend>
             <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[-27.2092052, -49.6401092]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-27.2092052, -49.6401092]} />
+          </Map>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf" id="uf">
+                Estado (UF)
+              </label>
+              <select name="uf" id="uf">
+                <option onChange={handleSelectUf} value="0">
+                  Selecione uma UF
+                </option>
+                {ufs.map((uf) => (
+                  <option key={uf} value={uf}>
+                    {uf}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="city" id="city">
+                Cidade
+              </label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma cidade</option>
+              </select>
+            </div>
+          </div>
         </fieldset>
+
         <fieldset>
           <legend>
-            <h2>Items de coleta</h2>
+            <h2>Ítens de coleta</h2>
+            <span>Selecione um ou mais itens abaixo</span>
           </legend>
+
+          <ul className="items-grid">
+            {items.map((item) => (
+              <li key={item.id} className="">
+                <img src={item.image_url} alt={item.title} />
+                <span>{item.title}</span>
+              </li>
+            ))}
+          </ul>
         </fieldset>
+        <button type="submit">Cadastrar ponto de coleta</button>
       </form>
     </div>
   );

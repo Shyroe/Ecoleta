d9f2124f987d29df1f7a3ebafad79e4ca2abd453
Shyroe <leo.henrique747@gmail.com>
2020-06-05 23:04:15 -0300

    aula 3 completed

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, ChangeEvent } from "react";
+import React, { useEffect, useState, ChangeEvent, FormEvent } from "react";
 import "./styles.css";
 import logo from "../../assets/logo.svg";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, TileLayer, Popup, Marker } from "react-leaflet";
 import { LeafletMouseEvent } from "leaflet";
@@ -29,13 +29,26 @@ const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [ufs, setUfs] = useState<string[]>([]); // ufs é do tipo array de strings
   const [cities, setCities] = useState<string[]>([]); // ufs é do tipo array de strings
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
+
   const [selectedUf, setSelectedUf] = useState("0");
   const [selectedCity, setSelectedCity] = useState("0");
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
     0,
     0,
   ]);
-  console.log("items: ", items);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    whatsapp: "",
+  });
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+  //History
+  const history = useHistory();
 
   useEffect(() => {
     api.get("items").then((res) => {
@@ -70,6 +83,14 @@ const CreatePoint = () => {
       });
   }, [selectedUf]);
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // console.log("position: ", position);
+      const { latitude, longitude } = position.coords;
+      setInitialPosition([latitude, longitude]);
+    });
+  }, []);
+
   // change UF
   function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
     console.log(event.target.value);
@@ -88,6 +109,52 @@ const CreatePoint = () => {
   function handleMapClick(event: LeafletMouseEvent) {
     setSelectedPosition([event.latlng.lat, event.latlng.lng]);
   }
+
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value, // o [] de [name] serve para podermos utilizar uma variável como propriedade, ao invés de apenas name de forma estática
+    });
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex((item) => item === id);
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter((item) => item !== id);
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    // console.log("handlesubmit");
+    const { name, email, whatsapp } = formData;
+    const items = selectedItems;
+    const [latitude, longitude] = selectedPosition;
+    const city = selectedCity;
+    const uf = selectedUf;
+    const data = {
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf,
+      items,
+    };
+    console.log("data: ", data);
+    api.post("/points", data).then((res) => {
+      console.log(res);
+      alert("Ponto de coleta criado");
+
+      history.push("/"); // Redirect to home page
+    });
+  }
+
   return (
     <div id="page-create-point">
       <header>
@@ -97,7 +164,7 @@ const CreatePoint = () => {
           Voltar para home
         </Link>
       </header>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>
           Cadastro do <br /> ponto de coleta
         </h1>
@@ -107,18 +174,33 @@ const CreatePoint = () => {
           </legend>
           <div className="field">
             <label htmlFor="name">Nome da entidade</label>
-            <input type="text" name="name" id="name" />
+            <input
+              type="text"
+              onChange={handleInputChange}
+              name="name"
+              id="name"
+            />
           </div>
 
           <div className="field-group">
             <div className="field">
               <label htmlFor="email">E-mail</label>
-              <input type="email" name="email" id="email" />
+              <input
+                type="email"
+                onChange={handleInputChange}
+                name="email"
+                id="email"
+              />
             </div>
 
             <div className="field">
               <label htmlFor="whatsapp">Whatsapp</label>
-              <input type="text" name="whatsapp" id="whatsapp" />
+              <input
+                type="text"
+                onChange={handleInputChange}
+                name="whatsapp"
+                id="whatsapp"
+              />
             </div>
           </div>
         </fieldset>
@@ -131,7 +213,8 @@ const CreatePoint = () => {
 
           <Map
             onClick={handleMapClick}
-            center={[-27.2092052, -49.6401092]}
+            // center={[-27.2092052, -49.6401092]}
+            center={initialPosition}
             zoom={15}
           >
             <TileLayer
@@ -192,7 +275,11 @@ const CreatePoint = () => {
 
           <ul className="items-grid">
             {items.map((item) => (
-              <li key={item.id} className="">
+              <li
+                key={item.id}
+                className={selectedItems.includes(item.id) ? "selected" : ""}
+                onClick={() => handleSelectItem(item.id)}
+              >
                 <img src={item.image_url} alt={item.title} />
                 <span>{item.title}</span>
               </li>
